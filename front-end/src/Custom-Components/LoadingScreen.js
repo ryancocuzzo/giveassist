4e71997f02a2bb7b50380a582fe7f179e2c9e064
ryancocuzzo <rcocuzzo8@gmail.com>
2026-02-12 00:28:00 -0500

    delete old frontend

--- front-end/src/Custom-Components/LoadingScreen.js
+++
@@ -1,7 +0,0 @@
-import React, { Component } from "react";
-
-export default class Loadinggg extends Component {
-  render() {
-    return <div className="giffy" style={{height: window.innerHeight+ 'px', display: 'block'}}><div style={{width:'70%', textAlign: 'center', margin: '0 auto'}}><div></div><br/><h1 style={{ marginTop: window.innerHeight/3+ 'px', fontSize: '80px'}}>Loading ...</h1></div></div>;
-  }
-}
--- front-end/src/Helper-Files/firebase.js
+++
@@ -1,9 +0,0 @@
-// src/firebase.js
-import firebase from 'firebase';
-import vars from '../Helper-Files/variables.js';
-let config = vars.fb_config;
-  firebase.initializeApp(config);
-
-  export const provider = new firebase.auth.GoogleAuthProvider();
-export const auth = firebase.auth();
-export default firebase;
